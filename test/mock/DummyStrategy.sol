f711786a8c0cd0574519029d903ed746cee12c1a
rayeaster <rayeaster@hotmail.com>
2025-06-05 19:40:00 +0800

    ensure withdraw fee charged during withdraw request claim

--- test/mock/DummyStrategy.sol
+++ test/mock/DummyStrategy.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract DummyStrategy {
     uint256 _totalAssets;
     uint256 _assetsInCollection;
@@ -34,18 +36,24 @@ contract DummyStrategy {
     }
 
     function totalAssets() external view returns (uint256 totalManagedAssets) {
-        return _totalAssets;
+        return _totalAssets > 0 ? _totalAssets : ERC20(_asset).balanceOf(address(this));
     }
 
     function assetsInCollection() external view returns (uint256 inCollectionAssets) {
         return _assetsInCollection;
     }
 
-    function allocate(uint256 amount) external {}
+    function allocate(uint256 amount) external {
+        ERC20(_asset).transferFrom(_vault, address(this), amount);
+    }
 
-    function collect(uint256 amount) external {}
+    function collect(uint256 amount) external {
+        ERC20(_asset).transferFrom(address(this), _vault, amount);
+    }
 
-    function collectAll() external {}
+    function collectAll() external {
+        ERC20(_asset).transferFrom(address(this), _vault, ERC20(_asset).balanceOf(address(this)));
+    }
 
     function strategist() external view returns (address strategist) {
         return _strategist;
